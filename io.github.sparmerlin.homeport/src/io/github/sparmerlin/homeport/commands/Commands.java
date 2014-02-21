1b82f9679cc0cdd7304c90f0a6e30e1b64d15500
sparmerlin <lukejohnnorris@gmail.com>
2014-02-21 17:20:49 +1100

    Added Beta Repository.

--- io.github.sparmerlin.homeport/src/io/github/sparmerlin/homeport/commands/Commands.java
+++ io.github.sparmerlin.homeport/src/io/github/sparmerlin/homeport/commands/Commands.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+/** Beta Version **/
 public class Commands implements CommandExecutor {
 
 	private HomePort plugin;
